f74ec950694e723d71b68d3dcb2ff8cdbbf3b477
Latesh Padaliya <lateshpadaliya412@gmail.com>
2026-07-19 10:14:15 +0530

    Add application validation middleware

---
+++ middleware/applicationValidator.js
@@ -0,0 +1,9 @@
+function validateCreateApplication(req, res, next) {
+  const { company, role } = req.body || {};
+  if (!company || !role) {
+    return res.status(400).json({ error: 'company and role are required' });
+  }
+  next();
+}
+
+module.exports = { validateCreateApplication };
